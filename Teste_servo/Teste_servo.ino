992cdf534b6ba63c2b8c03887a5de9dcba7d065f
Sidnei de Souza Martins <ssmartins@gmail.com>
2017-10-17 21:57:20 -0200

    teste do servo pelo codigo do robotcore

--- Teste_servo/Teste_servo.ino
+++ Teste_servo/Teste_servo.ino
@@ -1,22 +1,29 @@
-#include <Servo.h> //Este comando inclui a biblioteca Servo neste código
-Servo Servo1; //Identifica um objeto do tipo Servo chamado Servo1
-int Recebido; //Variável que armazenará o valor recebido pela serial
-int posicao; //Variável que armazenará as posições do servo
-void setup(){
-Servo1.attach(3); //Pino onde o servo deve estar colocado
-Serial.begin(9600); //Inicia a comunicação serial com taxa de 9600
+#include <Servo.h> 
+#define SERVO 3 // Porta Digital 6 PWM
+ 
+Servo s; // Variável Servo
+int pos; // Posição Servo
+ 
+void setup ()
+{
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(0); // Inicia motor posição zero
 }
-void loop(){
-if (Serial.available() > 0) { //Se algo vier pela porta serial...
-Recebido = Serial.read(); //O que receber, armazenar na variavel Recebido
-if (Recebido == '+'){ //se receber o sinal de + pela serial faça:
-posicao = posicao + 10; //adicione 10 à variável incremento
-Serial.println(posicao); //mostra no Monitor Serial o valor da posição
-}
-if (Recebido == '-'){ //se receber o sinal de - pela serial faça:
-posicao = posicao - 10; //subtraia 10 à variável incremento
-Serial.println(posicao); //mostra no Monitor Serial o valor da posição
-}
-Servo1.write(posicao); //Escreve a posição no servo
-}
+ 
+void loop()
+{
+  for(pos = 0; pos < 90; pos++)
+  {
+    s.write(pos);
+    //delay(15);
+  }
+  
+  delay(1000);
+  
+  for(pos = 90; pos >= 0; pos--)
+  {
+    s.write(pos);
+    delay(15);
+  }
 }
