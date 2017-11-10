cf8e2a9f7d940d4fce86eaaf9ec846ec37104b9e
Sidnei de Souza Martins <ssmartins@gmail.com>
2017-11-09 22:07:54 -0200

    Adicionado ProjetoEstacionamento

---
+++ ProjetoLCD/ProjetoLCD.ino
@@ -0,0 +1,266 @@
+
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <PubSubClient.h>
+
+#include <AsyncDelay.h>
+
+AsyncDelay delay_update;
+#define UPDATE_TIME 10000
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xF1, 0x35 }; // Endereço MAC utilizado na placa Ethernet
+const int rs = 9, en = 8, d4 = 6, d5 = 5, d6 = 4, d7 = 3; // Pinos de conexão do LCD
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int TOTAL_VAGAS = 3; // Total de vagas do estacionamento
+#define POSICAO_TEXTO1 10 // Posição de escrita do número de vagas livres/ocupadas
+#define POSICAO_TEXTO2 10
+
+#define STATUSLEDRED A5 // O led vermelho de status da conexão MQTT está no pino A5
+#define STATUSLEDGREEN A4 // O led verde de status da conexão MQTT está no pino A4
+#define STATUSLEDYELLOW A3 // O led azul de status da conexão MQTT está no pino A3
+
+#define APAGALCD 7 // Pino que controla o brilho do LCD
+
+#define NUM_PISCADAS 10 // Define quantas vezes o led de sinalização de comunicação MQTT irá piscar para cada mensagem
+
+int blinkLed = LOW;
+int contador = NUM_PISCADAS;
+unsigned long tempo = NUM_PISCADAS;
+unsigned long tempoinicial = 0;
+int blinkLedStatus = LOW;
+
+#define CONNECTED 0x00
+#define DISCONNECTED 0x01
+int mqttStateMachine;
+#define RECONNECT_TIME 5000 // Intervalo de tempo entre tentativas de reconexão ao servidor MQTT
+unsigned long lastConnectAttempt = 0;
+
+int qntVagasLivres = 0;
+int qntVagasOcupadas = 0;
+int totalVagas = 0;
+
+int vagas[TOTAL_VAGAS]; // Nesse vetor é registrado o status de cada vaga do estacionamento, vagas[i-1]=1 indica que vaga a vaga i está livre, 0 indica que a vaga i está ocupada
+int linkStatus[TOTAL_VAGAS];
+void callback(char *topic, byte *payload, unsigned int length);
+
+EthernetClient ethClient;
+
+// Dados do MQTT Cloud
+#define MQTT_SERVER "m14.cloudmqtt.com"
+#define MQTT_ID "Magal"
+#define MQTT_USER "asjjbr"
+#define MQTT_PASSWORD "1234"
+#define MQTT_PORT 11222
+#define MQTT_TOPIC "vagas/#"
+
+PubSubClient client(MQTT_SERVER, MQTT_PORT, callback, ethClient);
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  delay_update.expire();
+  delay_update.repeat();
+  // Calcula o tamanho da string com o nome do tópico MQTT
+  int topicSize = 0;
+  for(int i = 0;topic[i]!=0; i++)
+    topicSize = i + 1;
+
+  // Converte os caracteres que indicam o número da vaga em inteiro
+  int i = 6;
+  int valor = 0;
+  while(i < topicSize){
+    valor *= 10;
+    valor += (int)topic[i]-48;
+    i++;
+  }
+  valor--;
+
+  // atualiza o status da vaga i
+  if(valor<TOTAL_VAGAS){
+    if(length){
+      vagas[valor] = (int)payload[0]-48;
+      linkStatus[valor] = 1;
+    }
+    else{
+      vagas[valor] = 0;
+      linkStatus[valor] = 0;
+    }
+    blinkLed = HIGH;
+    tempoinicial = millis();
+    contador += NUM_PISCADAS;
+  }
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("OCUPADAS: ");
+  lcd.setCursor(0, 1);
+  lcd.print("LIVRES: ");
+  
+  pinMode(STATUSLEDRED, OUTPUT);
+  pinMode(STATUSLEDGREEN, OUTPUT);
+  pinMode(STATUSLEDYELLOW, OUTPUT);
+  pinMode(APAGALCD, OUTPUT);
+  digitalWrite(STATUSLEDRED, HIGH);
+  digitalWrite(STATUSLEDGREEN, LOW);
+  digitalWrite(STATUSLEDYELLOW, LOW);
+  digitalWrite(APAGALCD, LOW);
+  
+  for (int i = 0; i < TOTAL_VAGAS; i++) {
+    vagas[i] = 0;
+    linkStatus[i] = 0;
+  }
+  digitalWrite(STATUSLEDYELLOW, HIGH);
+  lastConnectAttempt = millis();
+  Ethernet.begin(mac);
+  if (client.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD)) {
+   
+    // Conecta no topic para receber mensagens
+    client.subscribe("senai-code-xp/vagas/#");
+
+    mqttStateMachine = CONNECTED;
+    digitalWrite(STATUSLEDGREEN, HIGH);
+    digitalWrite(STATUSLEDRED, LOW);
+  } else {
+    
+
+    mqttStateMachine = DISCONNECTED;
+    digitalWrite(STATUSLEDGREEN, LOW);
+    digitalWrite(STATUSLEDRED, HIGH);
+  }
+  delay_update.start(UPDATE_TIME, AsyncDelay::MILLIS);
+}
+
+void loop() {
+  client.loop();
+  int aux1 = qntVagasLivres;
+  int aux2 = qntVagasOcupadas;
+  totalVagas = atualizaTotalVagas();
+  qntVagasLivres = atualizaVagasLivres();
+  qntVagasOcupadas = atualizaVagasOcupadas(totalVagas);
+
+  if (aux1!=qntVagasLivres || aux2!=qntVagasOcupadas)
+    refreshLCD(qntVagasLivres, qntVagasOcupadas);
+
+  if (delay_update.isExpired()){
+    lcd.noDisplay();
+  }
+  else{
+    lcd.display();
+  }
+ 
+  switch (mqttStateMachine) {
+    case CONNECTED: // Caso o estado atual seja conectado, testa se o servidor continua online
+      if (!client.connected()) {
+        mqttStateMachine = DISCONNECTED; // Caso o servidor esteja offline, muda o estado da máquina para desconectado
+        digitalWrite(STATUSLEDGREEN, LOW); // Apaga o led verde
+        digitalWrite(STATUSLEDRED, HIGH); // Acende o led vermelho
+        lastConnectAttempt = 0;
+      }
+      break;
+    case DISCONNECTED:
+      if (millis() - lastConnectAttempt > RECONNECT_TIME) { // Caso o estado atual seja desconectado e o intervalo entre conexões tenha passado, tenta reconectar
+        digitalWrite(STATUSLEDYELLOW, HIGH);
+        lastConnectAttempt = millis(); // Registra a hora da tentativa de reconexão
+        if (client.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD)) {
+          // Se a conexão foi bem sucedida, assina o topic para receber/enviar mensagens
+          client.subscribe(MQTT_TOPIC);
+
+          mqttStateMachine = CONNECTED; // Muda o estado da máquina para conectado
+          digitalWrite(STATUSLEDGREEN, HIGH); // Acende o led verde
+          digitalWrite(STATUSLEDRED, LOW); // Apaga o led vermelho
+        }
+        digitalWrite(STATUSLEDYELLOW, LOW);
+      }
+      break;
+  }
+  
+  if(blinkLed){ // Rotina para piscar o led sempre que uma mensagem for publicada/recebida
+    if(contador<=0){
+      blinkLed = LOW;
+      contador = NUM_PISCADAS;
+      digitalWrite(STATUSLEDYELLOW, blinkLed);
+    }
+    else{
+      if(millis() - tempoinicial > tempo){
+        blinkLedStatus = !blinkLedStatus;
+        digitalWrite(STATUSLEDYELLOW, blinkLedStatus);
+        tempoinicial = millis();
+        contador--;
+      }
+    }
+  }
+}
+
+int atualizaVagasLivres() {
+  int aux = 0;
+  for (int i = 0; i < TOTAL_VAGAS; i++) {
+    aux += (vagas[i]*linkStatus[i]);
+  }
+  //if(aux!=qntVagasLivres){
+  //Serial.print("Livres: ");
+  //Serial.println(aux);}
+  return aux;
+}
+
+int atualizaVagasOcupadas(int total) {
+  int aux = 0;
+  for (int i = 0; i < TOTAL_VAGAS; i++) {
+    aux += (vagas[i]*linkStatus[i]);
+  }
+  aux = total - aux;
+  //if(aux!=qntVagasOcupadas){
+  //Serial.print("Ocupadas: ");
+  //Serial.println(aux);}
+  return aux;
+}
+
+int atualizaTotalVagas(){
+  int aux = 0;
+  for (int i = 0; i < TOTAL_VAGAS; i++) {
+    aux += linkStatus[i];
+  }
+  //if(aux!=totalVagas){
+  //Serial.print("Total: ");
+  //Serial.println(aux);}
+  return aux;
+}
+
+void refreshLCD(int livres, int ocupadas){
+  if(livres > 0){
+    
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print("OCUPADAS: ");
+    lcd.setCursor(0, 1);
+    lcd.print("LIVRES: ");
+    
+    lcd.setCursor(POSICAO_TEXTO1, 0);
+    lcd.print("  ");
+    lcd.setCursor(POSICAO_TEXTO1, 0);
+    lcd.print(qntVagasOcupadas);
+
+    lcd.setCursor(POSICAO_TEXTO2, 1);
+    lcd.print("  ");
+    lcd.setCursor(POSICAO_TEXTO2, 1);
+    lcd.print(qntVagasLivres);
+  }
+  else{
+    if(ocupadas>0){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("NAO HA VAGAS");
+    }
+    else{
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("SEM COMUNICAÇÃO");
+    }
+  }
+}
+
--- ProjetoVaga/ProjetoVaga.ino
+++ ProjetoVaga/ProjetoVaga.ino
@@ -1,24 +1,32 @@
-#include <SPI.h>
-#include <UIPEthernet.h>
-#include <PubSubClient.h>
+//#define OFFLINE 0 // Macro para retirar o código de conexão com o MQTT (apenas para testes)
+#ifndef OFFLINE
+  #include <SPI.h>
+  #include <UIPEthernet.h>
+  #include <PubSubClient.h>
+#endif
+
 #include <Ultrasonic.h>
 
 #define STATUSLEDRED 6 // O led vermelho de status da conexão MQTT está no pino 7
 #define STATUSLEDGREEN 5 // O led verde de status da conexão MQTT está no pino 9
 #define STATUSLEDYELLOW 7 // O led azul de status da conexão MQTT está no pino 8
 
-#define VAGAOCUPADALED 3
-#define VAGALIVRELED 2
+#define VAGAOCUPADALED 4 // Definição do pino de saída do led que indica que a vaga está ocupada
+#define VAGALIVRELED 3 // Definição do pino de saída do led que indica que a vaga está livre
 
-Ultrasonic ultrasonic(8, 9);
+#define pino_trigger 8 // Definição do pino de trigger do sensor ultrassonico
+#define pino_echo 9 // Definição do pino de echo do sensor ultrassonico
 
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+// Definição dos estados da máquina que controla a conexão/reconexão com o servidor MQTT
 #define CONNECTED 0x00
 #define DISCONNECTED 0x01
 
 // Update these with values suitable for your network.
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0xff};
 
-#define NUM_PISCADAS 10
+#define NUM_PISCADAS 10 // Define quantas vezes o led de sinalização de comunicação MQTT irá piscar para cada mensagem
 
 int mqttStateMachine;
 int blinkLed = LOW;
@@ -27,33 +35,42 @@ unsigned long tempo = NUM_PISCADAS;
 unsigned long tempoinicial = 0;
 int blinkLedStatus = LOW;
 
-#define RECONNECT_TIME 5000
+#define RECONNECT_TIME 5000 // Intervalo de tempo entre tentativas de reconexão ao servidor MQTT
 unsigned long lastConnectAttempt = 0;
 
 #define DISTANCIA 30
-//#define HISTERESE 1
+#define HISTERESE 1
 
 #ifndef HISTERESE
   #define MIN_DIST DISTANCIA
   #define MAX_DIST MIN_DIST
 #else
   #define MIN_DIST DISTANCIA
-  #define MAX_DIST DISTANCIA+3
+  #define MAX_DIST DISTANCIA+10
 #endif
 
 #define VAGA_OCUPADA 0x01
 #define VAGA_LIVRE 0x02
-#define MQTT_TOPIC "senai-code-xp/vagas/kit36"
+
+// Dados do servidor e tópico MQTT
+#define MQTT_SERVER "m14.cloudmqtt.com"
+#define MQTT_ID "Magal"
+#define MQTT_USER "asjjbr"
+#define MQTT_PASSWORD "1234"
+#define MQTT_PORT 11222
+#define MQTT_TOPIC "senai-code-xp/vagas/1"
+
 int vagaStateMachine = VAGA_LIVRE;
 char vagaStatus = '1';
 int mqttUpdate = 0;
 
+#ifndef OFFLINE
 void callback(char *topic, byte *payload, unsigned int length);
 
 EthernetClient ethClient;
 
 // Dados do MQTT Cloud
-PubSubClient client("m10.cloudmqtt.com", 16367, callback, ethClient);
+PubSubClient client(MQTT_SERVER, MQTT_PORT, callback, ethClient);
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -61,6 +78,7 @@ void callback(char *topic, byte *payload, unsigned int length)
   tempoinicial = millis();
   contador += NUM_PISCADAS;
 }
+#endif
 
 void setup() {
   
@@ -77,16 +95,16 @@ void setup() {
   digitalWrite(STATUSLEDYELLOW, LOW);
   digitalWrite(VAGAOCUPADALED, LOW);
   digitalWrite(VAGALIVRELED, HIGH);
-  
-  Ethernet.begin(mac);
 
+  #ifndef OFFLINE
+  Ethernet.begin(mac);
+  #endif
+
+  #ifndef OFFLINE
   digitalWrite(STATUSLEDYELLOW, HIGH);
   lastConnectAttempt = millis();
-  if (client.connect("Magal", "coiktbwj", "zAhaklL2atGf"))
+  if (client.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD, MQTT_TOPIC, 0, true, ""))
   {
-    // Envia uma mensagem para o cloud no topic portao
-    //client.publish("portao", 65);
-
     // Conecta no topic para receber mensagens
     client.subscribe(MQTT_TOPIC);
     
@@ -98,6 +116,7 @@ void setup() {
     digitalWrite(STATUSLEDGREEN, LOW);
     digitalWrite(STATUSLEDRED, HIGH);
   }
+  #endif
   digitalWrite(STATUSLEDYELLOW, LOW);
 }
 
@@ -126,31 +145,50 @@ void loop() {
       break;
   }
   
+  #ifndef OFFLINE
   switch(mqttStateMachine){
-    case CONNECTED:
+    case CONNECTED: // Caso o estado atual seja conectado, testa se o servidor continua online
       if(!client.connected()){
-        mqttStateMachine = DISCONNECTED;
-        digitalWrite(STATUSLEDGREEN, LOW);
-        digitalWrite(STATUSLEDRED, HIGH);
+        mqttStateMachine = DISCONNECTED; // Caso o servidor esteja offline, muda o estado da máquina para desconectado
+        digitalWrite(STATUSLEDGREEN, LOW); // Apaga o led verde
+        digitalWrite(STATUSLEDRED, HIGH); // Acende o led vermelho
         lastConnectAttempt = 0;
       }
-      break;
-    case DISCONNECTED:
-      if(millis() - lastConnectAttempt > RECONNECT_TIME){
-        if (client.connect("Magal", "coiktbwj", "zAhaklL2atGf")){
-          // Conecta no topic para receber mensagens
-          client.subscribe(MQTT_TOPIC);
-        
-          mqttStateMachine = CONNECTED;
-          digitalWrite(STATUSLEDGREEN, HIGH);
-          digitalWrite(STATUSLEDRED, LOW);
-          lastConnectAttempt = millis();
+      else{
+        if(mqttUpdate){ // Se o flag indicando uma mensagem a ser publicada no MQTT estiver setado envia o status da vaga
+          boolean flag;
+          flag = client.publish(MQTT_TOPIC, vagaStatus, true);
+          if(flag){
+            blinkLed = HIGH;
+            tempoinicial = millis();
+            contador += NUM_PISCADAS;
+            mqttUpdate = 0;
+          }
         }
       }
       break;
+    case DISCONNECTED:
+      Serial.println("Desconectado");
+      if(millis() - lastConnectAttempt > RECONNECT_TIME){ // Caso o estado atual seja desconectado e o intervalo entre conexões tenha passado, tenta reconectar
+        Serial.println("Tentando conectar");
+        digitalWrite(STATUSLEDYELLOW, HIGH);
+        lastConnectAttempt = millis(); // Registra a hora da tentativa de reconexão
+        if (client.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD, MQTT_TOPIC, 0, true, "")){
+          Serial.println("Conectado");
+          // Se a conexão foi bem sucedida, assina o topic para receber/enviar mensagens
+          client.subscribe(MQTT_TOPIC);
+        
+          mqttStateMachine = CONNECTED; // Muda o estado da máquina para conectado
+          digitalWrite(STATUSLEDGREEN, HIGH); // Acende o led verde
+          digitalWrite(STATUSLEDRED, LOW); // Apaga o led vermelho
+        }
+        digitalWrite(STATUSLEDYELLOW, LOW);
+      }
+      break;
   }
-
-  if(blinkLed){
+  #endif
+  
+  if(blinkLed){ // Rotina para piscar o led sempre que uma mensagem for publicada/recebida
     if(contador<=0){
       blinkLed = LOW;
       contador = NUM_PISCADAS;
@@ -159,25 +197,10 @@ void loop() {
     else{
       if(millis() - tempoinicial > tempo){
         blinkLedStatus = !blinkLedStatus;
-        digitalWrite(STATUSLEDYELLOW, blinkLed);
+        digitalWrite(STATUSLEDYELLOW, blinkLedStatus);
         tempoinicial = millis();
         contador--;
       }
     }
   }
-  else
-    digitalWrite(STATUSLEDYELLOW, blinkLed);
-
-  if(mqttUpdate){
-    boolean flag;
-    flag = client.publish(MQTT_TOPIC, vagaStatus, true);
-    if(flag){
-      blinkLed = HIGH;
-      tempoinicial = millis();
-      contador += NUM_PISCADAS;
-      mqttUpdate = 0;
-    }
-  }
-
-  
 }
